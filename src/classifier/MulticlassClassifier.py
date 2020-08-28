b418b22ea072f736be5882697aef7ba91cc490b1
Mauro <afjcjsbx@gmail.com>
2020-08-28 15:26:37 +0200

    added different scalers

--- src/classifier/MulticlassClassifier.py
+++ src/classifier/MulticlassClassifier.py
@@ -222,19 +222,19 @@ class MulticlassClassifier(AbstractClassifier):
         """
         Data scaling/normalization
         """
-        #scaler = prep.MinMaxScaler(feature_range=
+        scaler = prep.MinMaxScaler(feature_range=(0, 1))
         #scaler = prep.MaxAbsScaler()
         #scaler = prep.QuantileTransformer(output_distribution='uniform')
         #scaler = prep.PowerTransformer(method='yeo-johnson')
-        scaler = prep.PowerTransformer(method='box - cox')
+        #scaler = prep.PowerTransformer(method='box - cox')
 
 
         # using following transformation:
         #  X_std = (X - X.min(axis=0)) / (X.max(axis=0) - X.min(axis=0))
         #  X_scaled = X_std * (max - min) + min
         self.__LOG.info(f"[SCALING] Data scaling using {type(scaler).__qualname__}")
-        #scaler.fit(self.training.set_x)
-        scaler = Normalizer().fit(self.training.set_x)  # fit does nothing.
+        scaler.fit(self.training.set_x)
+        #scaler = Normalizer().fit(self.training.set_x)  # fit does nothing.
 
         self.training.set_x = scaler.transform(self.training.set_x)
         self.test.set_x = scaler.transform(self.test.set_x)
